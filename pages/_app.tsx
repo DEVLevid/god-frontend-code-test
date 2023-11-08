3c8a0714e28e749600b00ab62d7e4d1044baef69
DEVLevid <levidlima70@gmail.com>
2023-11-08 16:11:15 -0300

    Feat: add provider and themepicker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { HelloWorld } from "../src/components/HelloWorld";
+import { StyleProvider, ThemePicker } from "vcc-ui";
 import "../public/css/styles.css";
 import React from "react";
 
 function HomePage() {
   return (
-    <React.StrictMode>
-      <HelloWorld />
-    </React.StrictMode>
+    <StyleProvider>
+      <ThemePicker variant="light">
+        <HelloWorld />
+      </ThemePicker>
+    </StyleProvider>
   );
 }
 
