1d77f40e9bf009329607e71af07712fec76cd0af
DEVLevid <levidlima70@gmail.com>
2023-11-11 16:36:43 -0300

    feat: create home page container and their styles

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Spacer, Text } from "vcc-ui";
+import { useCars } from "../hooks/getCars";
+import CarCard from "./carCard";
+
+import styles from "../../public/css/container.module.css";
 
 export const HelloWorld: React.FC = () => {
-  return <h1>Hello World!</h1>;
+  const { cars } = useCars();
+  return (
+    <div className={styles.container}>
+      <Text variant='cook'>Recharge Models</Text>
+      <Spacer/>
+      <div className={styles.cardContainer}>
+      {cars.map((car) => (
+          <CarCard car={car} />
+        ))}
+      CTS</div>
+    </div>
+  );
 };
